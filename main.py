2989d9310bdcea16937a76bfc3e87e6a2104ea62
darinconnorseiler <122124226+darinconnorseiler@users.noreply.github.com>
2023-01-07 11:52:46 +0400

    Retrieving one individual post

--- main.py
+++ main.py
@@ -15,6 +15,11 @@ class Post(BaseModel):
 my_posts = [{"title": "title of post 1", "content" :"content of post 1", "id" : 1},
             {"title": "favourite foods", "content" :"I like pizza", "id": 2}]
 
+def find_post(id):
+    for p in my_posts:
+        if p['id'] == id:
+            return p
+
 @app.get("/")
 def root():
     return {"message": "Hello World"}
@@ -29,4 +34,11 @@ def create_posts(post: Post):
     post_dict['id'] = randrange(0, 100000)
     my_posts.append(post_dict)
     return {"data": post_dict}
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+
+    post = find_post(id)
+    print(post)
+    return{"post_detail": post}
 
