653885d5165fe3d036cddc7b6c7d8cf8656a73cc
darinconnorseiler <122124226+darinconnorseiler@users.noreply.github.com>
2023-01-07 11:14:25 +0400

    Fixing Naming conventions and adding an array

--- main.py
+++ main.py
@@ -11,17 +11,20 @@ class Post(BaseModel):
     published: bool = True
     rating: Optional[int] = None
 
+my_posts = [{"title": "title of post 1", "content" :"content of post 1", "id" : 1},
+            {"title": "favourite foods", "content" :"I like pizza", "id": 2}]
+
 @app.get("/")
 def root():
     return {"message": "Hello World"}
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "This is your posts"}
+    return {"data": my_posts}
 
-@app.post("/createposts")
+@app.post("/posts")
 def create_posts(post: Post):
     print(post)
     print (post.dict())
     return {"data": post}
-#title str, content str
+
