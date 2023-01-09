fe25f41f537c5d7c2b918c3c83280638b1210199
darinconnorseiler <122124226+darinconnorseiler@users.noreply.github.com>
2023-01-09 22:15:17 +0400

    Fixed psycopg2 import.

--- app/main.py
+++ app/main.py
@@ -10,7 +10,18 @@ class Post(BaseModel):
     title: str
     content: str
     published: bool = True
-    rating: Optional[int] = None
+
+try:
+    conn = psycopg2.connect(host='localhost',
+                            database='fastapi',
+                            user='postgres',
+                            password='metermaid',
+                            cursor_factory=RealDictCursor)
+    cursor = conn.cursor()
+    print("Database connection was successful")
+except Exception as error:
+    print("Connecting to database failed")
+    print("Error", error)
 
 my_posts = [{"title": "title of post 1", "content" :"content of post 1", "id" : 1},
             {"title": "favourite foods", "content" :"I like pizza", "id": 2}]
