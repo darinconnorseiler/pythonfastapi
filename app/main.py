bd4e0c985b32c22bbfa6cff105b7942441173431
darinconnorseiler <122124226+darinconnorseiler@users.noreply.github.com>
2023-01-10 15:42:56 +0400

    fixed connection to postgres error

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 from typing import Optional
-from fastapi import FastAPI, Response, status, HTTPException
+from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
+from sqlalchemy.orm import Session
 from . import models
 from .database import engine, SessionLocal
 
@@ -57,6 +58,11 @@ def find_index_post(id):
 @app.get("/")
 def root():
     return {"message": "Hello World"}
+
+##Test function
+@app.get("/sqlalchemy")
+def test_posts(db: Session = Depends(get_db)):
+    return{"status": "success"}
 
 @app.get("/posts")
 def get_posts():
