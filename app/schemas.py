4af27ecdcc384210046a686c94ca5afdc7ce145a
darinconnorseiler <122124226+darinconnorseiler@users.noreply.github.com>
2023-01-11 15:32:15 +0400

    Adding a user model with post request

--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from datetime import datetime
 
 class PostBase(BaseModel):
@@ -12,6 +12,19 @@ class PostCreate(PostBase):
 ##Response
 class Post(PostBase):
     id: int
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str 
+
+#Response
+class UserOut(BaseModel):
+    id: int 
+    email: EmailStr
     created_at: datetime
 
     class Config:
